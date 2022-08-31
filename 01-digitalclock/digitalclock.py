a5433c65cc80f35052e9317e3aa6864f608ecab4
tri3vt <tri3vt@gmail.com>
2022-08-31 18:03:42 +0700

    add 1 project

---
+++ 01-digitalclock/digitalclock.py
@@ -0,0 +1,25 @@
+'''
+Pre-requirement
+Python version >=3.9
+https://www.python.org/download/mac/tcltk/
+
+Run: python3 digitalclock.py
+'''
+
+from cProfile import label
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+root=Tk()
+root.title("Digital Clock")
+
+def clock():
+    string=strftime('%H:%M:%S:%p')
+    label.config(text=string)
+    label.after(1000,clock)
+
+label=Label(root, font=("Digital",100), background="black", foreground="green")
+label.pack(anchor="center")
+clock()
+root.mainloop()
